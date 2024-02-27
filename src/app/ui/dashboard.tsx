db346f897cb3cab4557b31c2f1c2e6b3cada236a
Arpit Singh <adreshsingh271527@gmail.com>
2024-02-27 08:44:28 +0530

    settlement page

---
+++ src/app/ui/dashboard.tsx
@@ -0,0 +1,26 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+
+import { FaFire } from 'react-icons/fa';
+
+// const SideIcons = (icons: any) => {
+//     return (<div>{icons.icon}</div>);
+// }
+
+const Tryit = ({ icon }) => {
+    return (
+        <div className='w-full bg-purple-400 p-4 border-2 border-slate-400 rounded-md hover:bg-blue-200 hover:text-black focus:ring-1 outline-none'>
+            {icon}
+        </div>)
+}
+
+export const Dashboard = () => {
+    return (
+        <div className="fixed flex h-screen w-16 bg-green-500 flex-col">
+
+            <Tryit icon={<FaFire fontSize={'24px'} />} />
+            <Tryit icon={<BsFillLightningFill fontSize={'24px'} />} />
+            <Tryit icon={<BsGearFill fontSize={'24px'} />} />
+            <Tryit icon={<FaFire fontSize={'24px'} />} />
+        </div>
+    );
+}
