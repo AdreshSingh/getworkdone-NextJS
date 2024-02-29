f9a9e67f7de928dadb50f3169569eeae67d3da75
Arpit Singh <adreshsingh271527@gmail.com>
2024-02-29 08:51:19 +0530

    Still changement in layout of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import { Dashboard } from "./ui/dashboard";
 import { HomePage } from "./ui/HomePage";
+import { NavBar } from "./ui/navbar";
 
 export default function Home() {
   return (
-    <div className='w-screen'><Dashboard /><HomePage /></div>
+    <div className='w-screen'>
+      <Dashboard />
+      <NavBar />
+      <HomePage />
+      </div>
   );
 }
