12e62451c38ffa78da6dc1f6bb2ff2593a8fffe3
Arpit Singh <adreshsingh271527@gmail.com>
2024-03-01 08:09:25 +0530

    little more UI changes

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -5,7 +5,7 @@ export const NavBar=()=>{
         <div className="grid grid-cols-2 place-content-center p-7">
                 <h1 className='text-center'>GetWorkDone</h1>
                 <p className="flex flex-row justify-end ml-10">
-                    <Link href={'/pages/login/login'}>
+                    <Link href={'/pages/login'} className='hover:text-green-300 text-red-500 ring-1 ring-red-500 hover:ring-green-400 rounded-3xl duration-75 transition-all'>
                     <BiPowerOff fontSize="26px" />
                     </Link>
                 </p>
