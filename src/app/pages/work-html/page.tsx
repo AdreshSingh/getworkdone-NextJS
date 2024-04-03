3d57c5a654584ecbea53665225f033a53b920970
Arpit Singh <adreshsingh271527@gmail.com>
2024-04-04 04:59:02 +0530

    adding various tasks like word,html,

--- src/app/pages/work-html/page.tsx
+++ src/app/pages/work-html/page.tsx
@@ -4,34 +4,32 @@ export default  () => {
     const data: task[] = [
         {
             id: 1,
-            taskCategory: "MS OFFICE",
-            taskTitle: "Paragraph",
+            taskCategory: "HTML",
+            taskTitle: "Simple Page",
             taskBy: "Devank",
-            taskDate: "29/07/2024"
+            taskDate: "17/04/2024"
         },
         {
             id: 2,
-            taskCategory: "MS OFFICE",
-            taskTitle: "Paragraph",
+            taskCategory: "HTML",
+            taskTitle: "Simple Page-2",
             taskBy: "Devank",
-            taskDate: "30/07/2024"
+            taskDate: "17/04/2024"
         }
     ]
     return (
         <div className="pl-16">
             {
-                data.map((item) => {
-                    return (
-                        <>
-                            <div className="bg-purple-300 text-white font-extrabold p-2 w-">{
-                                item.taskCategory
-                            }</div>
-                            <div className="grid grid-cols-1 md:grid-cols-3 bg-indigo-500">
-                                <Task key={item.id} taskTitle={item.taskTitle} taskBy={item.taskBy} taskDate={item.taskDate} />
-                            </div>
-                        </>
-                    )
-                })
+ data.map((item) => {
+    return (
+            <>
+         <div className="bg-purple-300 text-white font-extrabold p-2 w-">
+            {item.taskCategory}</div>
+            <div className="grid grid-cols-1 md:grid-cols-3 bg-indigo-500">
+            <Task key={item.id} taskTitle={item.taskTitle} taskBy={item.taskBy} taskDate={item.taskDate} />
+            </div>
+            </>)
+            })
             }
         </div>
     );
