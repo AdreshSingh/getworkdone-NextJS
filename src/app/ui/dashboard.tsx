29d9fd1a2a6704e76ee84b7305968cf354a46469
Arpit Singh <adreshsingh271527@gmail.com>
2024-04-04 05:00:32 +0530

    some semantics changes to code

--- src/app/ui/dashboard.tsx
+++ src/app/ui/dashboard.tsx
@@ -1,16 +1,17 @@
-import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { BsPlus, BsFillLightningFill, BsGearFill, BsFiletypeHtml, BsWordpress } from 'react-icons/bs';
 
 import { FaFire } from 'react-icons/fa';
 import { Avatar } from './Avatar';
+import Link from 'next/link';
 
 // const SideIcons = (icons: any) => {
 //     return (<div>{icons.icon}</div>);
 // }
 
-const Tryit = (icon:any) => {
+const Tryit = (data:any) => {
     return (
         <div className='w-full bg-purple-400 p-4 border-2 border-slate-400 rounded-md hover:bg-blue-200 hover:text-black focus:ring-1 outline-none'>
-            {icon.icons}
+            {data.icon}
         </div>)
 }
 
@@ -18,6 +19,8 @@ export const Dashboard = () => {
     return (
         <div className="fixed flex h-screen w-16 bg-green-500 flex-col">
             <Tryit icon={<Avatar>Devank</Avatar>} />
+            <Link href={'pages/work-html'}><Tryit icon={<BsFiletypeHtml fontSize={"24px"} />} /></Link>
+            <Link href={'pages/work-html'}><Tryit icon={<BsWordpress fontSize={"24px"} />} /></Link>
             <Tryit icon={<FaFire fontSize={'24px'} />} />
             <Tryit icon={<BsFillLightningFill fontSize={'24px'} />} />
             <Tryit icon={<BsGearFill fontSize={'24px'} />} />
