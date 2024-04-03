18c4999f90bb57af7e9343e5af3fa2eb09872d77
Arpit Singh <adreshsingh271527@gmail.com>
2024-04-04 05:03:10 +0530

    some UI changes for website logo

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -4,7 +4,7 @@ export const NavBar=()=>{
     const user= null;
     return(
         <div className="grid grid-cols-2 place-content-center p-7">
-                <h1 className='text-center'>GetWorkDone</h1>
+                <h1 className='text-center text-4xl'>GetWorkDone</h1>
                 <p className="flex flex-row justify-end ml-10">
                     {
                         user ===null ? <Link href={'/pages/signup'} className=' text-red-500 ring-1 ring-red-500 hover:ring-green-400 rounded-3xl duration-75 transition-all'>
