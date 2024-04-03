62b3a7c0d6a2fbede67c8ec29abddc1cc1fe8348
Arpit Singh <adreshsingh271527@gmail.com>
2024-04-04 04:59:33 +0530

    some UI changes

--- src/app/ui/Avatar.tsx
+++ src/app/ui/Avatar.tsx
@@ -1,6 +1,4 @@
 export const Avatar=(props:any)=>{
     const chukd=props.children ;
-    return <div className="">
-        {chukd[0].toUpperCase()}
-    </div>
+    return (<div className=" text-center font-bold rounded-lg border-white border-2">{chukd[0].toUpperCase()}</div>);
 }
